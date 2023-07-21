dae718df5e721963831f1acfd1463a562e301b73
Casper Boon <casper.boon@uwa.edu.au>
2023-07-21 11:26:30 +0800

    test update for windows build

--- fv-source/build_aed-fv.bat
+++ fv-source/build_aed-fv.bat
@@ -1,6 +1,7 @@
 REM @echo off
 
 cd fv-source
+call "%INTEL_HPCKIT_INSTALL_PATH%\compiler\%INTEL_COMPILER_VERSION%\env\vars.bat"
 
 for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VS_BASE_PATH=%%a\Common7
 set VS_BASE_PATH=%programfiles(x86)%\Microsoft Visual Studio\2019\Community\Common7
